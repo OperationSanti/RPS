5779aa8b4dfa65b982be8ab0ee4d1c392cd54dcc
OperationSanti <128191513+OperationSanti@users.noreply.github.com>
2023-05-08 02:16:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var player = 1;
     var winner = 0;
     var colors = {};
@@ -8,16 +8,45 @@ $(document).ready(function(){
     var gameStartTime;
     var player1Time = 0;
     var player2Time = 0;
-    var gameTimerInterval;
-    var player1TimerInterval;
-    var player2TimerInterval;
+    var timerInterval;
 
-    $(".cell").each(function(){
+    var hasResultBeenDisplayed = false;
+
+    $(".cell").each(function() {
         $(this).attr("id", count);
         $(this).attr("data-player", 0);
-        $(this).attr("disabled", true); // disable the cells initially
+        $(this).attr("disabled", true);
         count++;
     });
+
+    $(".cell").click(function() {
+        if ($("#start").attr("disabled") !== "disabled" || isValid($(this).attr("id")) === false || winner !== 0) {
+            return;
+        }
+        if (isValid($(this).attr("id"))) {
+            $(this).css("background-color", colors[player]);
+            $(this).attr("data-player", player);
+            if (checkWin(player)) {
+                clearInterval(timerInterval);
+                winner = player;
+                var playerTime = player === 1 ? player1Time * 1000 : player2Time * 1000;
+                var formattedTime = formatTime(playerTime)
+                $(".cell").attr("disabled", true);
+                $("#pause, #reset, #rematch").removeAttr("disabled");
+            } else {
+                player *= -1;
+            }
+        }
+    });
+    
+
+    $("#rematch").click(function() {
+        resetGame();
+        $("#start").removeAttr("disabled");
+        hasResultBeenDisplayed = false;
+        $(".result-message").remove();
+    });
+
 
     function startGame() {
         player = 1;
@@ -25,12 +54,8 @@ $(document).ready(function(){
         player1Time = 0;
         player2Time = 0;
         gameStartTime = new Date();
-        clearInterval(gameTimerInterval);
-        clearInterval(player1TimerInterval);
-        clearInterval(player2TimerInterval);
-        gameTimerInterval = setInterval(updateGameTimer, 1000);
-        player1TimerInterval = setInterval(updatePlayer1Timer, 1000);
-        player2TimerInterval = setInterval(updatePlayer2Timer, 1000);
+        clearInterval(timerInterval);
+        timerInterval = setInterval(updateTimers, 1000);
         $("#pause").removeAttr("disabled"); // enable the Pause button
         $("#reset").removeAttr("disabled"); // enable the Reset button
         $(".cell").css("background-color", "white");
@@ -40,15 +65,32 @@ $(document).ready(function(){
 
     function resetGame() {
         startGame();
-        clearInterval(gameTimerInterval);
-        clearInterval(player1TimerInterval);
-        clearInterval(player2TimerInterval);
+        clearInterval(timerInterval);
         $("#player1-time").text("Player 1 time: 00:00:00");
         $("#player2-time").text("Player 2 time: 00:00:00");
         $("#game-time").text("Game time: 00:00:00");
         $(".cell").attr("disabled", true); // disable the cells
+        $(".cell").css("background-color", "white"); // set all cells to white
+        $(".cell").attr("data-player", 0); // reset the data-player attribute to 0
         $("#pause").attr("disabled", true); // disable the Pause button
         $("#reset").attr("disabled", true); // disable the Reset button
+        if (winner === 1) {
+            $("#red").css("color", "white"); // set the red color to white
+        } else if (winner === -1) {
+            $("#yellow").css("color", "white"); // set the yellow color to white
+        }
+        winner = 0; // reset the winner variable
+    }
+    
+    
+
+    function updateTimers() {
+        updateGameTimer();
+        if (player === 1 && winner === 0) {
+            updatePlayer1Timer();
+        } else if (player === -1 && winner === 0) {
+            updatePlayer2Timer();
+        }
     }
 
     function updateGameTimer() {
@@ -58,35 +100,14 @@ $(document).ready(function(){
     }
 
     function updatePlayer1Timer() {
-        if (player === 1 && winner === 0) {
-            player1Time++;
-            $("#player1-time").text("Player 1 time: " + formatTime(player1Time * 1000));
-            if (checkWin(player)) {
-                clearInterval(gameTimerInterval);
-                clearInterval(player1TimerInterval);
-                clearInterval(player2TimerInterval);
-                var winTime = formatTime(player1Time * 1000);
-                $("#winning-time").text("Player 1 wins! Time: " + winTime);
-                winner = player;
-            }
-        }
+        player1Time++;
+        $("#player1-time").text("Player 1 time: " + formatTime(player1Time * 1000));
     }
         
     function updatePlayer2Timer() {
-        if (player === -1 && winner === 0) {
-            player2Time++;
-            $("#player2-time").text("Player 2 time: " + formatTime(player2Time * 1000));
-            if (checkWin(player)) {
-                clearInterval(gameTimerInterval);
-                clearInterval(player1TimerInterval);
-                clearInterval(player2TimerInterval);
-                var winTime = formatTime(player2Time * 1000);
-                $("#winning-time").text("Player 2 wins! Time: " + winTime);
-                winner = player;
-            }
-        }
+        player2Time++;
+        $("#player2-time").text("Player 2 time: " + formatTime(player2Time * 1000));
     }
-    
 
     function formatTime(time) {
         var date = new Date(time);
@@ -105,128 +126,101 @@ $(document).ready(function(){
         resetGame();
         $("#start").removeAttr("disabled"); // enable the Start button after the game is reset
     });
-    
     $("#pause").click(function() {
         if ($("#pause").text() === "Pause") {
-            clearInterval(gameTimerInterval);
-            clearInterval(player1TimerInterval);
-            clearInterval(player2TimerInterval);
+            clearInterval(timerInterval);
             $(".cell").attr("disabled", true); // disable the cells
             $("#pause").text("Resume");
         } else {
-            gameTimerInterval = setInterval(updateGameTimer, 1000);
-            player1TimerInterval = setInterval(updatePlayer1Timer, 1000);
-            player2TimerInterval = setInterval(updatePlayer2Timer, 1000);
+            timerInterval = setInterval(updateTimers, 1000);
             $(".cell").removeAttr("disabled"); // enable the cells
             $("#pause").text("Pause");
         }
     });
-    
-    $(".cell").click(function(){
-        if ($("#start").attr("disabled") !== "disabled" || isValid($(this).attr("id")) === false) {
-            return; // disable the cells before Start button is pressed or if it's not a valid move
+
+    $(".cell").click(function() {
+        if ($("#start").attr("disabled") !== "disabled" || isValid($(this).attr("id")) === false || winner !== 0) {
+            return;
         }
-        if(isValid($(this).attr("id"))){
+        if (isValid($(this).attr("id"))) {
             $(this).css("background-color", colors[player]);
             $(this).attr("data-player", player);
-            if(checkWin(player)){
-                alert(colors[player] + " has won! " + formatTime(player1Time * 1000));
+            if (checkWin(player)) {
+                clearInterval(timerInterval);
                 winner = player;
-                clearInterval(gameTimerInterval);
-                clearInterval(player1TimerInterval);
-                clearInterval(player2TimerInterval);
-            } else {
-                if (player === 1) {
-                    updatePlayer2Timer();
-                } else {
-                    updatePlayer1Timer();
+                var playerTime = player === 1 ? player1Time * 1000 : player2Time * 1000;
+                var formattedTime = formatTime(playerTime);
+                $(".cell").attr("disabled", true);
+                $("#pause, #reset, #rematch").removeAttr("disabled");
+                if (winner === 1) {
+                    $("#red").css("color", "white"); // set the red color to white
+                } else if (winner === -1) {
+                    $("#yellow").css("color", "white"); // set the yellow color to white
                 }
+            } else {
                 player *= -1;
             }
         }
     });
     
-    function isValid(n){
-        var id = parseInt(n);
-        if(winner !== 0){
+
+    function isValid(id){
+        if ($("#" + id).attr("data-player") !== "0") {
             return false;
         }
-        if($("#" + id).attr("data-player") === "0"){
-            if(id >= 35){
-                return true;
-            }
-            if($("#" + (id + 7)).attr("data-player") !== "0"){
-                return true;
+        if (id < 35) {
+            return $("#" + (parseInt(id) + 7)).attr("data-player") !== "0";
+        }
+        return true;
+    }
+
+    function checkWin(player) {
+        for (var row = 0; row < 6; row++) {
+            for (var col = 0; col < 7; col++) {
+                var cellId = (row * 7) + col;
+                if (checkDirection(cellId, player, 1, 0) || // horizontal
+                    checkDirection(cellId, player, 0, 1) || // vertical
+                    checkDirection(cellId, player, 1, 1) || // diagonal up
+                    checkDirection(cellId, player, 1, -1)) { // diagonal down
+                    clearInterval(timerInterval);
+                    winner = player;
+                    var playerTime = player === 1 ? player1Time * 1000 : player2Time * 1000;
+                    var formattedTime = formatTime(playerTime);
+                    var $resultList = $('#result-list');
+                    var winnerIndex = $resultList.children().length + 1;
+                    $("<li>").text(colors[player] + " won with a time of: " + formattedTime).appendTo("#result-list");
+                    $(".cell").attr("disabled", true);
+                    $("#pause, #reset, #rematch").removeAttr("disabled");
+                    if (winner === 1) {
+                        $("#red").css("color", "white"); // set the red color to white
+                    } else if (winner === -1) {
+                        $("#yellow").css("color", "white"); // set the yellow color to white
+                    }
+                    return true;
+                }
             }
         }
         return false;
     }
     
-    function checkWin(p){
-        //check rows
-        var chain = 0;
-        for(var i = 0; i < 42; i+=7){
-            for(var j = 0; j < 7; j++){
-                var cell = $("#" + (i+j));
-                if(cell.attr("data-player") == p){
-                    chain++;
-                }else{
-                    chain=0;
-                }
-    
-                if(chain >= 4){
+
+    function checkDirection(cellId, player, rowDir, colDir) {
+        var count = 0;
+        var row = Math.floor(cellId / 7);
+        var col = cellId % 7;
+
+        while (row >= 0 && row < 6 && col >= 0 && col < 7) {
+            if ($("#" + (row * 7 + col)).attr("data-player") === player.toString()) {
+                count++;
+                if (count === 4) {
                     return true;
                 }
+            } else {
+                count = 0;
             }
-            chain = 0;
+            row += rowDir;
+            col += colDir;
         }
-    
-        //check columns
-        chain = 0;
-        for(var i = 0; i < 7; i++){
-            for(var j = 0; j < 42; j+=7){
-                var cell = $("#" + (i + j));
-                if(cell.attr("data-player") == p){
-                    chain++;
-                }else{
-                    chain = 0;
-                }
-    
-                if(chain >= 4){
-                    return true;
-                }
-            }
-            chain = 0;
-        }
-    
-        //check diagonals
-       
-        var topLeft = 0;
-        var topRight = topLeft + 3;
-    
-        for(var i = 0; i < 3; i++){
-            for(var j = 0; j < 4; j++){
-                if($("#" + topLeft).attr("data-player") == p
-                && $("#" + (topLeft + 8)).attr("data-player") == p
-                && $("#" + (topLeft + 16)).attr("data-player") == p
-                && $("#" + (topLeft + 24)).attr("data-player") == p){
-                    return true;
-                }
-    
-                if($("#" + topRight).attr("data-player") == p
-                && $("#" + (topRight + 6)).attr("data-player") == p
-                && $("#" + (topRight + 12)).attr("data-player") == p
-                && $("#" + (topRight + 18)).attr("data-player") == p){
-                    return true;
-                }
-    
-                topLeft++;
-                topRight = topLeft + 3;
-            }
-            topLeft = i * 7 + 7;
-            topRight = topLeft + 3;
-        }
-    
         return false;
     }
-});        
+});
